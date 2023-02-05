b5b416be87636febc238595a8f9181ed903a8468
unknown <gabriel.cscanes@hotmail.com>
2023-02-05 14:32:12 -0300

    Estrutura base - Menu - Carrossel

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    }, 
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+
+    disableOnInteraction: false,
+    
+});
