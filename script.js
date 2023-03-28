c73ee04bc0594934762f6733a0c99e82651d8158
unknown <gabriel.cscanes@hotmail.com>
2023-03-28 00:10:06 -0300

    Ajustes de responsividade

--- script.js
+++ script.js
@@ -12,3 +12,44 @@ var swiper = new Swiper(".swiper", {
     disableOnInteraction: false,
     
 });
+
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+const events = ['touchstart', 'click'];
+
+
+dropdownMenus.forEach(menu => {
+    events.forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick);
+    });
+});
+
+function handleClick (event){
+    event.preventDefault();
+    this.classList.add('ativo');
+    
+    outsideClick(this, events, () => {
+        this.classList.remove('ativo');
+    });
+}
+
+function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside';
+
+    if(!element.hasAttribute(outside)){
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handleOutsideClick);
+        })
+        element.setAttribute(outside, '');
+    }
+
+    function handleOutsideClick(event) {
+        if(!element.contains(event.target)){
+            element.removeAttribute(outside);
+            events.forEach(userEvent => {
+                html.removeEventListener(userEvent, handleOutsideClick);
+            });
+            callback();
+        }
+    }
+}
